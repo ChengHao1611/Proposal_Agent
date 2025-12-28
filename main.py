b5455521438166c68127127b7173adffb2648756
tianlong0527 <liangquan0527@gmail.com>
2025-12-28 23:13:32 +0800

    change the start file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from agent import send_message_to_agent
+import os
+from Linebot import app
+
 import logging
 
 logging.basicConfig(
@@ -8,9 +10,5 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    get_response = send_message_to_agent("4", """
-    https://sites.google.com/view/ncku-ilink/%E9%A6%96%E9%A0%81/
-                          """, 1) 
-    
-    print(get_response)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
